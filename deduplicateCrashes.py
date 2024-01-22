7ff659e376e5c6a2d9b6f811e28f17944580c42f
Yang Jongmun <sheepbelldoor.dev@gmail.com>
2024-01-22 18:51:44 +0900

    Update deduplicateCrashes.py

--- deduplicateCrashes.py
+++ deduplicateCrashes.py
@@ -32,31 +32,32 @@ UNIFUZZ = [
     ]
 
 FTS = [
-    'boringssl-2016-02-12',
-    'c-ares-CVE-2016-5180',
-    'guetzli-2017-3-30',
-    'harfbuzz-1.3.2',
-    'json-2017-02-12',
-    'lcms-2017-03-21',
-    'libarchive-2017-01-04',
-    'libjpeg-turbo-07-2017',
-    'libpng-1.2.56',
-    'libpng-1.2.56-structure-aware',
-    'libssh-2017-1272',
-    'libxml2-v2.9.2',
-    'openssl-1.0.1f',
-    'openssl-1.0.2d',
-    'openssl-1.1.0c-bignum',
-    'openssl-1.1.0c-x509',
-    'openthread-2018-02-27-ip6',
-    'openthread-2018-02-27-radio',
-    'pcre2-10.00',
-    'proj4-2017-08-14',
-    're2-2014-12-09',
-    'sqlite-2016-11-14',
-    'vorbis-2017-12-11',
-    'woff2-2016-05-06',
-    'wpantund-2018-02-27',
+    'boringssl-2016-02-12',#
+    # 'c-ares-CVE-2016-5180',
+    'c-ares',#
+    'guetzli-2017-3-30',#
+    'harfbuzz-1.3.2',#
+    'json-2017-02-12',#
+    'lcms-2017-03-21',#
+    'libarchive-2017-01-04',#
+    'libjpeg-turbo-07-2017',#
+    'libpng-1.2.56',#
+    # 'libpng-1.2.56-structure-aware',
+    'libssh-2017-1272',#
+    'libxml2-v2.9.2',#
+    'openssl-1.0.1f',#
+    # 'openssl-1.0.2d',
+    # 'openssl-1.1.0c-bignum',
+    'openssl-1.1.0c-x509',#
+    'openthread-2018-02-27-ip6',#
+    'openthread-2018-02-27-radio',#
+    'pcre2-10.00',#
+    'proj4-2017-08-14',#
+    're2-2014-12-09',#
+    'sqlite-2016-11-14',#
+    'vorbis-2017-12-11',#
+    'woff2-2016-05-06',#
+    'wpantund-2018-02-27',#
     ]
 
 totalUniqueBugs = []
@@ -187,6 +188,7 @@ def main():
                      'tcpdump',
                      'vorbis-2017-12-11'
                      ]
+    targetProgram = FTS
     for target in targetProgram:
         deduplicateCrashes(target)
 
